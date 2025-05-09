9ff1f22227d2a274429ca0432cd4733d7eae1507
leoV0 <leo.cigale@gmail.com>
2025-05-09 10:52:43 +0200

    Nettoyage js

--- script.js
+++ script.js
@@ -12,21 +12,19 @@ const calendrierDuCrayon = {
 };
 
 // const tousLesMois = Object.keys(calendrierDuCrayon);
-//   console.log(tousLesMois);
+// console.log(tousLesMois);
+// console.log(calendrierDuCrayon[mois]); // Retourne 10 avec septembre
 
 const hautDuCrayon = `  /\\ \n /__\\`;
 const crayon = " ||||";
 const gommeBase = " |__|";
-// const gommeUnEtage = " |__|";
 const gommeDeuxEtage = " |__| \n |  | \n |__|";
 
 function recupMois(mois) {
-  // console.log(calendrierDuCrayon[mois]); // Retourne 10 avec septembre
   console.log(hautDuCrayon);
   for (let i = 0; i < calendrierDuCrayon[mois]; i++) {
     console.log(crayon);
   }
-  // console.log(calendrierDuCrayon[mois]);
   if (calendrierDuCrayon[mois] > 4) {
     console.log(gommeBase);
   } else {
@@ -34,26 +32,3 @@ function recupMois(mois) {
   }
 }
 recupMois("octobre");
-
-// la gomme a deux étages en début d'année scolaire, mais se réduit à un seul étage à partir du mois de mars.
-
-// retourne un crayon à 10 étages avec une gomme à 2 étages
-//  /\
-// /__\
-// ||||
-// ||||
-// ||||
-// ||||
-// ||||
-// ||||
-// ||||
-// ||||
-// ||||
-// ||||
-// |__|
-// |  |
-// |__|
-
-// const found = calendrierDuCrayon.find((element) => element > 5);
-
-// console.log(found);
